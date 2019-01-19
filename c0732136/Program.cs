daade611d650f6aaa6f590d011282ef5e49ba419
visualstudio <visualstudio@122-PC32>
2019-01-18 21:07:34 -0500

    change1

--- c0732136/Program.cs
+++ c0732136/Program.cs
@@ -10,6 +10,49 @@ namespace c0732136
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+
+            StudentInformation _john = new StudentInformation("john", 3.8, 20);
+
+            StudentInformation _Mark = new StudentInformation("Mark", 3.8, 20);
+
+            StudentInformation _Steve = new StudentInformation("Steve", 3.8, 20);
+
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.8, 20);
+
+            Console.ReadLine();
+
         }
     }
-}
+
+    }
+
+    class StudentInformation
+
+    {
+
+        public string name;
+
+
+
+        public double GPA;
+
+        public int attendence;
+
+        public StudentInformation(string aName, double aGPA, int dAttendence)
+
+        {
+
+            name = aName;
+
+            GPA = aGPA;
+
+            attendence = dAttendence;
+        }
+
+    }
+
+         
+    
+    
+
